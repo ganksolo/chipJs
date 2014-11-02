0a1632acc7b4b7a93eab067686fad9645689ad07
ganksolo <studiocms@live.cn>
2014-11-02 15:59:42 +0800

    增加统计代码

---
+++ src/log.js
@@ -0,0 +1,9 @@
+var log ＝ function(tag, url) {
+    var date = new Date().getTime(),
+        label = 'log__' + date,
+        c = window[label] = new Image();
+    // 将onload 和onerror 事件处理程序指定为同一个函数, 无论是什么响应，只要请求完成，便释放该对象。
+    c.onload = c.onerror = function(){ window[label] = null; };
+    c.src =  url ? url : "http://p2.youxi.bdimg.com/statics/tj.gif?type=clickStat&clickFlag=" + tag + "&from=" + encodeURIComponent(location.href) + "&time=" + encodeURIComponent(date.toString());
+    c = null;
+}
